4534a222092eee5c1d4e82b674dcd3f150a3d1b8
Robin Van den Broeck <robin.broeck@gmail.com>
2018-11-14 15:11:54 +0100

    Fetch the games from the backend and show them as json

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createApiUrl } from "./api"
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fetching: false,
+      games: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ fetching: true });
+    console.log(createApiUrl);
+    fetch(createApiUrl("games"))
+      .then(res => res.json())
+      .then(json => this.setState({
+        fetching: false,
+        games: json
+      }));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <>
+        {this.state.fetching && <div>Loading</div>}
+        {this.state.games.map(game => <pre key="{game.id}">{JSON.stringify(game)}</pre>)}
+      </>
+    )
   }
 }
 
