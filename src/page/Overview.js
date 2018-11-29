81f3281fe9e9ec6a607b3d855fe1b1348ca9e11a
Robin Van den Broeck <robin.broeck@gmail.com>
2018-11-29 15:24:49 +0100

    Added error handling to overview

--- src/page/Overview.js
+++ src/page/Overview.js
@@ -8,45 +8,55 @@ import { createApiUrl } from "../api";
 import Game from "../components/Game";
 
 class Overview extends Component {
-  constructor() {
-    super();
-    this.state = {
-      fetching: false,
-      games: []
-    };
-  }
+  state = {
+    fetching: false,
+    games: []
+  };
 
   async componentDidMount() {
     this.setState({ fetching: true });
-    const result = await fetch(createApiUrl("games"));
-    const json = await result.json();
-    this.setState({
-      fetching: false,
-      games: json
-    });
+    try {
+      const result = await fetch(createApiUrl("games"));
+      const json = await result.json();
+      this.setState({
+        fetching: false,
+        games: json
+      });
+    } catch (e) {
+      this.setState({
+        loading: false,
+        error: e
+      });
+    }
   }
 
   render() {
+    const { loading, error, games } = this.state;
+
+    if (loading) {
+      return <div>Loading</div>;
+    }
+    if (error) {
+      return <div>Error: {this.state.error.toString()}</div>;
+    }
+
     return (
-      <>
-        {this.state.fetching && <div>Loading</div>}
-        <div style={{ padding: 20 }}>
-          <Grid container spacing={24}>
-            {this.state.games.map(game => (
-              <Grid item xs={6} key={game.id}>
-                <Link to={`/game/${game.id}`}>
-                  <Game key={game.id} game={game} />
-                </Link>
-              </Grid>
-            ))}
-          </Grid>
-          <Link to="/game/add">
-            <Fab color="primary">
-              <AddIcon />
-            </Fab>
-          </Link>
-        </div>
-      </>
+      <div style={{ padding: 20 }}>
+        <Grid container spacing={24}>
+          {games.map(game => (
+            <Grid item xs={6} key={game.id}>
+              <Link to={`/game/${game.id}`}>
+                <Game key={game.id} game={game} />
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+        <Link to="/game/add">
+          <Fab color="primary">
+            <AddIcon />
+          </Fab>
+        </Link>
+      </div>
     );
   }
 }
